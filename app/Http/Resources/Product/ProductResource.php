809af1cc1aa4f92814ea54396c3617b7f7a5ea69
Paul Carlo Tordecilla <ptordecilla@posible.net>
2018-07-19 14:11:08 +0800

    Edited Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,13 @@ class ProductResource extends JsonResource
             'name'  => $this->name,
             'description'   => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'total_price' => round((1 - ($this->discount/100)) * $this->discount,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('start')/$this->reviews->count(),2) : 'No Rating Yet', 
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
 
         ];
     }
